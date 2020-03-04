3328b35d6b6fdb869f9b50debffb7f79d844c861
Joseph McGovern <joseph.mcgovern@workiva.com>
2020-03-04 12:46:29 -0600

    Syncing files

--- update.sh
+++ update.sh
@@ -8,7 +8,6 @@ cp ~/.gitconfig .
 sed -i "" 's/name = .*/name = First Last/' .gitconfig
 sed -i "" 's/email = .*/email = youremail@email.com/' .gitconfig
 cp ~/local_scripts/.git_update.sh .
-cp ~/local_scripts/.sync_master.py .
 cp ~/local_scripts/.open_pull.py .
 cp ~/.tmux.conf .
 ls ~/.vim/bundle > vim_plugins.txt
